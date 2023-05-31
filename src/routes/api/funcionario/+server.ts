8496136a2a82dbca1203f0f6672f32b44b264c5c
Lucas A. Ouverney <realfakenerd@users.noreply.github.com>
2023-05-31 18:14:10 +0000

    try fix #5

--- src/routes/api/funcionario/+server.ts
+++ src/routes/api/funcionario/+server.ts
@@ -4,29 +4,10 @@ import type { RequestHandler } from "./$types";
 const url = 'http://187.60.56.72:9191/funcionario'
 export const GET = (async ({ fetch, setHeaders }) => {
     setHeaders({
-        'cache-control': 'max-age=3600'
+        'cache-control': 'max-age=60'
     })
     const res = await fetch(url);
     const data = (await res.json())
 
     return json(data);
-}) satisfies RequestHandler;
-
-export const POST = (async ({request, fetch}) => {
-    const fdata = await request.json();
-    await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(fdata)
-    })
-    return new Response(JSON.stringify(fdata), {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-}) satisfies RequestHandler;
-
-export const PUT = (async ({request,fetch}) => {
-    const body = await request.json();
-    console.log(body)
-    return json('body')
 }) satisfies RequestHandler;
